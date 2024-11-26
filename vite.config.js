c461bf43c1f3e49f0c962f73c1a1e0fcc3c4e1e2
Allan <altonallan47@gmail.com>
2024-11-26 19:47:00 +0100

    Update vite.config AGAIN

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import postcssImport from 'postcss-import';
-import postcssNesting from 'postcss-nesting';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        postcssImport(),
-        postcssNesting(),
-      ],
-    },
-  },
-  server: {
-    hmr: {
-      overlay: false, // Disable error overlay in the browser
-    },
-  },
-});
+})
